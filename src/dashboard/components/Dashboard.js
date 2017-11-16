c95b4acc89dae4d77259fc8e21d6f8015791086a
João Marcelo Souza <joaomarcelo9393@gmail.com>
2017-11-16 12:36:55 -0300

    Redirect to login if user not authenticated
    Previously, the user could go to the dashboard by typing /dashboard in the URL.
    
    Now, even if they does that, they'll be redirected to the login page.

--- src/dashboard/components/Dashboard.js
+++ src/dashboard/components/Dashboard.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 
 class Dashboard extends Component {
+  constructor(props) {
+    if (!props.authenticated) {
+      props.history.push('/');
+    }
+    super(props);
+  }
+
   componentDidMount() {
     this.props.projectsGetAll();
   }
--- src/dashboard/containers/Dashboard.js
+++ src/dashboard/containers/Dashboard.js
@@ -1,9 +1,12 @@
 import { connect } from 'react-redux';
 import Dashboard from '../components/Dashboard';
+import { getUsername } from '../../auth/AuthSelectors';
 import * as AuthActions from '../../auth/AuthActions';
 import * as DashboardActions from '../DashboardActions';
 
-const mapStateToProps = state => ({});
+const mapStateToProps = state => ({
+  authenticated: getUsername(state) != null,
+});
 
 const mapDispatchToProps = {
   handleLogout: AuthActions.authLogoutSubmitted,
