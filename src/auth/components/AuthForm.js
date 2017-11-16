8510216c68ec9d2336a1483db96f921296f51ece
João Marcelo Souza <joaomarcelo9393@gmail.com>
2017-11-16 15:43:49 -0300

    Style login form

--- src/auth/components/AuthForm.js
+++ src/auth/components/AuthForm.js
@@ -18,12 +18,53 @@ class AuthForm extends Component {
   render() {
     const { loading, valid } = this.props;
     return (
-      <form onSubmit={this.handleSubmit}>
-        <input disabled={loading} name="username" ref={(i) => { this.usernameInput = i; }} />
-        <input disabled={loading} type="password" ref={(i) => { this.passwordInput = i; }} />
-        <input disabled={loading} type="submit" />
-        {valid || <p>Invalid credentials</p>}
-      </form>
+      <section className="hero is-fullheight">
+        <div className="hero-body">
+          <div className="container has-text-centered">
+            <div className="column is-4 is-offset-4">
+              <h3 className="title has-text-grey">GitHub Projects</h3>
+              <p className="subtitle has-text-grey">Please login to proceed.</p>
+              <div className="box">
+                <form onSubmit={this.handleSubmit}>
+                  <div className="field">
+                    <div className="control">
+                      <input
+                        className="input"
+                        disabled={loading}
+                        name="username"
+                        placeholder="Username"
+                        ref={(i) => { this.usernameInput = i; }}
+                      />
+                    </div>
+                  </div>
+                  <div className="field">
+                    <div className="control">
+                      <input
+                        className="input"
+                        disabled={loading}
+                        type="password"
+                        placeholder="Password"
+                        ref={(i) => { this.passwordInput = i; }}
+                      />
+                    </div>
+                  </div>
+                  <div className="field">
+                    <div className="control">
+                      <input
+                        className="button is-primary is-fullwidth"
+                        disabled={loading}
+                        type="submit"
+                        value="Log in"
+                      />
+                    </div>
+                  </div>
+                  {valid || <p className="has-text-danger">Invalid credentials</p>}
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     );
   }
 }
